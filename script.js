01755189b61225407a76bf6433501bb8b6932094
Steve Black <SteveBlackecmpe@gmail.com>
2021-01-04 00:03:44 -0500

    WorkHours Array timeclass update function working, StartTimeSlot variable can adjust the start time by hours and minutes, TimeSlotInterval can adjust the length of each time slot, this allows the scheduled to be scaled for testing.

--- script.js
+++ script.js
@@ -10,135 +10,239 @@ $(document).ready(function () {//START JS Script after document load and ready--
     // let CurDate = moment().endOf('hour').fromNow();
     let CurDate = moment().format('dddd, MMMM Do');
     //86400 seconds in 24 hours
-    let TargetTime10pm = (10+12)*3600000;
+    // Test target time in ms, will become next top of hour after current time is determined.
+    //let TargetTime10pm = (11.5 + 12) * 3600000;
     //DateTime.local().toLocaleString(DateTime.DATE_FULL);
     // setTimeout(() => {use this for triggering next chack from where we are...
     //     CurDate = moment().format('dddd, MMMM, Do, sssss');
     //     $("#currentDay").text(CurDate);
     //     console.log(CurDate);
     // },500);
+    //
 
+    //test loop setinterval, updates the title 
     setInterval(function () {
-        // CurDate = moment().format('dddd, MMMM, Do, hh:mm:ss a');
-        // CurDate = moment().startOf('day').fromNow();
-        // CurDate = moment().endOf('hour').fromNow();
-        let CurLocDate = Date();
-        // let CurHr = CurDate.getHours();
-        // let CurSec = CurDate.getSeconds();
-        let CurMSec = Date.parse(CurLocDate);
-        // let CurTimeOfDayMS = Date.parse(CurDate);
-
-        CurMSTimeOfDay = moment().millisecond();
-        CurSecTimeOfDay = moment().seconds();
-        CurMinTimeOfDay = moment().minutes();
-        CurHrTimeofDay = moment().hours();
-
-        CurHMSmSTimeofDay = CurMSTimeOfDay + (CurSecTimeOfDay*1000) + (CurMinTimeOfDay*60000) + (CurHrTimeofDay*3600000);
-
-        let IntCurHMSmSTimeofDay = Math.floor(CurHMSmSTimeofDay);
-        let NextInterval = TargetTime10pm - IntCurHMSmSTimeofDay - 500;
-
-
-        $("#currentDay").text("MomCurDate->{" + CurDate + "} MomCurMillSec->{" + CurMSTimeOfDay + "} Date->{" + CurLocDate + "} DateParse->{" + CurMSec + "}");
-        // console.log(CurDate);
-        // console.log("CurHr " + CurHr);
-        // console.log("CurSec " + CurSec);
-        console.log("CurMSTimeOfDay " + CurMSTimeOfDay);
-        console.log("CurSecTimeOfDay" + CurSecTimeOfDay);
-        console.log("CurMinTimeOfDay " + CurMinTimeOfDay);
-        console.log("CurHrTimeofDay " + CurHrTimeofDay);
-        console.log("CurHMSmSTimeofDay " + CurHMSmSTimeofDay);
-        console.log("IntCurHMSmSTimeofDay " + IntCurHMSmSTimeofDay);
-        console.log("Target 10pm "+ TargetTime10pm);
-        console.log("Next Interval " + NextInterval);
+        // CurDate = moment().format('dddd, MMMM Do');
+        CurDate = moment().format('dddd, MMMM, Do, hh:mm:ss a');
+        $("#currentDay").text(CurDate);
     }, 500);
 
 
 
 
+    //test loop setinterval, updates the title 
+    // setInterval(function () {
+    //     // CurDate = moment().format('dddd, MMMM Do');
+    //     CurDate = moment().format('dddd, MMMM, Do, hh:mm:ss a');
+    //     // CurDate = moment().startOf('day').fromNow();
+    //     // CurDate = moment().endOf('hour').fromNow();
+    //     let CurLocDate = Date();
+    //     // let CurHr = CurDate.getHours();
+    //     // let CurSec = CurDate.getSeconds();
+    //     let CurMSec = Date.parse(CurLocDate);
+    //     // let CurTimeOfDayMS = Date.parse(CurDate);
+
+    //     CurMSTimeOfDay = moment().millisecond();
+    //     CurSecTimeOfDay = moment().seconds();
+    //     CurMinTimeOfDay = moment().minutes();
+    //     CurHrTimeofDay = moment().hours();
+
+    //     CurHMSmSTimeofDay = CurMSTimeOfDay + (CurSecTimeOfDay * 1000) + (CurMinTimeOfDay * 60000) + (CurHrTimeofDay * 3600000);
+
+    //     let IntCurHMSmSTimeofDay = Math.floor(CurHMSmSTimeofDay);
+    //     let NextInterval = TargetTime10pm - IntCurHMSmSTimeofDay - 500;
+
+    //     //If less than 1 hour
+    //     // let NextIntervalMins = Math.floor(NextInterval / (60000));
+    //     let NextIntervalMins = (NextInterval / (60000));
+    //     NextIntervalMins = NextIntervalMins.toFixed(2);
 
 
+    //     // $("#currentDay").text("MomCurDate->{" + CurDate + "} MomCurMillSec->{" + CurMSTimeOfDay + "} Date->{" + CurLocDate + "} DateParse->{" + CurMSec + "}");
+    //     $("#currentDay").text(CurDate);
+
+    //     // console.log(CurDate);
+    //     // console.log("CurHr " + CurHr);
+    //     // console.log("CurSec " + CurSec);
 
 
-    //OLD Static code for header load date and time.
-    // CurDate = moment().format('dddd, MMMM, Do, sssss');
-    // $("#currentDay").text(CurDate);
-    // console.log(CurDate);
+    //     // console.log("CurMSTimeOfDay " + CurMSTimeOfDay);
+    //     // console.log("CurSecTimeOfDay" + CurSecTimeOfDay);
+    //     // console.log("CurMinTimeOfDay " + CurMinTimeOfDay);
+    //     // console.log("CurHrTimeofDay " + CurHrTimeofDay);
+    //     // console.log("CurHMSmSTimeofDay " + CurHMSmSTimeofDay);
+    //     // console.log("IntCurHMSmSTimeofDay " + IntCurHMSmSTimeofDay);
+    //     // console.log("Target 11pm " + TargetTime10pm);
+    //     // console.log("Next Interval " + NextInterval);
+    //     // console.log("NextIntervalMins " + NextIntervalMins);
+    //     // console.log("StartTimeSlot " + StartTimeSlot);
 
 
+    // }, 500);
 
-    //Variables
+    //Variables--------------------------------------------------------------------------------
+
+
+    //Array of objects for
+    let WorkHours = [// basic start up content--------------------------------------------------------------------------------
+        {
+            Label: "9am",
+            EventText: "",
+            TimeClass: "",
+            StartTimeThresh: "",
+            SaveDate: ""
+        },
+        {
+            Label: "10am",
+            EventText: "",
+            TimeClass: "",
+            StartTimeThresh: "",
+            SaveDate: ""
+
+        },
+        {
+            Label: "11am",
+            EventText: "",
+            TimeClass: "",
+            StartTimeThresh: "",
+            SaveDate: ""
+        },
+        {
+            Label: "12am",
+            EventText: "",
+            TimeClass: "",
+            StartTimeThresh: "",
+            SaveDate: ""
+        },
+        {
+            Label: "1pm",
+            EventText: "",
+            TimeClass: "",
+            StartTimeThresh: "",
+            SaveDate: ""
+        },
+        {
+            Label: "2pm",
+            EventText: "",
+            TimeClass: "",
+            StartTimeThresh: "",
+            SaveDate: ""
+        }
+        , {
+            Label: "3pm",
+            EventText: "",
+            TimeClass: "",
+            StartTimeThresh: "",
+            SaveDate: ""
+        }
+        , {
+            Label: "4pm",
+            EventText: "",
+            TimeClass: "",
+            StartTimeThresh: "",
+            SaveDate: ""
+        }
+        , {
+            Label: "5pm",
+            EventText: "",
+            TimeClass: "",
+            StartTimeThresh: "",
+            SaveDate: ""
+        }
+    ];
+
 
     //TimeStartInterval - the starting time of the Scheduled List
     // variable defined in Milliseconds from 0;
     // 8 am = 8 hrs x 3600 seconds x 1000 milliseconds = 28800000
     // 1 hr = 3600000 ms
     //9am = 32400000
-    let TimeStartInterval = 0;
-
+    //Start time of the first time slot in the planner. Defined as Milliseconds
+    let StartTimeSlot = ((11 + 12) * 3600000 + (54*60000)   );//.0165 hrs = 1 min
     //TimeSlotQty - the number of allowed time slots in the day planner
-    let TimeSlotQty = 9;
-
-
-
+    let TimeSlotQty = WorkHours.length;
     //Time Slot Interval - variable is used to adjust the size the time slot, can be a small as 1 second, defined in milliseconds
-    let TimeSlotInterval = 1000;//1000 = 1 second, 3600 seconds = 1 hour, this affects the rate at which the background css class changes
-
-    //temp test function to update header
+    let TimeSlotInterval = 1000 * 3;//1000 = 1 second, 3600 seconds = 1 hour, this affects the rate at which the background css class changes
 
 
 
-    //Maybe this array needs to contain the amount of time for each
-    let WorkHours = [// basic start up content
-        {
-            Hr1: "9am",
-            EventText: "",
-            TimeClass: "",
-        },
-        {
-            Hr2: "10am",
-            EventText: "",
-            TimeClass: "",
-        },
-        {
-            Hr3: "11am",
-            EventText: "",
-            TimeClass: "",
-        },
-        {
-            Hr4: "12am",
-            EventText: "",
-            TimeClass: "",
-        },
-        {
-            Hr5: "1pm",
-            EventText: "",
-            TimeClass: "",
-        },
-        {
-            Hr6: "2pm",
-            EventText: "",
-            TimeClass: "",
-        }
-        , {
-            Hr7: "3pm",
-            EventText: "",
-            TimeClass: "",
-        }
-        , {
-            Hr8: "4pm",
-            EventText: "",
-            TimeClass: "",
-        }
-        , {
-            Hr9: "5pm",
-            EventText: "",
-            TimeClass: "",
-        }
-    ];
-
+    // console.log( WorkHours);
 
     //START LIST OF FUNCTIONS--------------------------------------FUNCTIONS START
+
+    //Reset Test Array Classes
+    function RTAC() {
+        for (let i = 0; i < WorkHours.length; i++) {
+            WorkHours[i].TimeClass = "";
+        }
+
+
+    };
+RTAC();
+
+    //Build Test Array Start Time Thresh Holds
+    function BTASTT() {
+        let TempStartTimeThresh = StartTimeSlot - TimeSlotInterval;//Sets the temp to 1 interval before start time.
+
+        for (let i = 0; i < WorkHours.length; i++) {
+            TempStartTimeThresh += TimeSlotInterval;//increments temp to start time on first iteration and next on all others
+            WorkHours[i].StartTimeThresh = TempStartTimeThresh;//sets the StartTimeThreshold for each array object
+        }
+
+
+    };
+    BTASTT();
+    // console.log(WorkHours);
+    // console.log(CurHMSmSTimeofDay);
+
+    //Update Time Class Past Present Future
+    function UTCPPF() {
+        for (i = 0; i < WorkHours.length; i++) {
+            CurHMSmSTimeofDay = 0;
+            CurHrTimeofDay = moment().hours();
+            CurHMSmSTimeofDay = CurHMSmSTimeofDay + (CurHrTimeofDay * 3600000);
+            CurMinTimeOfDay = moment().minutes();
+            CurHMSmSTimeofDay = CurHMSmSTimeofDay + (CurMinTimeOfDay * 60000);
+            CurSecTimeOfDay = moment().seconds();
+            CurHMSmSTimeofDay = CurHMSmSTimeofDay + (CurSecTimeOfDay * 1000);
+            CurMSTimeOfDay = moment().millisecond();
+            CurHMSmSTimeofDay = CurHMSmSTimeofDay + CurMSTimeOfDay;
+
+
+            // CurHMSmSTimeofDay = CurMSTimeOfDay + (CurSecTimeOfDay * 1000) + (CurMinTimeOfDay * 60000) + (CurHrTimeofDay * 3600000);
+
+            if (CurHMSmSTimeofDay <= (WorkHours[i].StartTimeThresh - 100)) {//past condition
+                WorkHours[i].TimeClass = "future";
+                WorkHours[i].EventText = CurHMSmSTimeofDay;
+            } else if ((CurHMSmSTimeofDay >= (WorkHours[i].StartTimeThresh - 100)) && (CurHMSmSTimeofDay < (WorkHours[i].StartTimeThresh + TimeSlotInterval - 100))) {//Present
+                WorkHours[i].TimeClass = "present";
+                WorkHours[i].EventText = CurHMSmSTimeofDay;
+            } else if (CurHMSmSTimeofDay > (WorkHours[i].StartTimeThresh - 100)) {
+                WorkHours[i].TimeClass = "past";
+                WorkHours[i].EventText = CurHMSmSTimeofDay;
+            } else {
+                WorkHours[i].TimeClass = "error";
+                WorkHours[i].EventText = CurHMSmSTimeofDay;
+            }
+        }
+
+    }
+    UTCPPF();
+    // console.log(WorkHours);
+    // console.log(CurHMSmSTimeofDay);
+    setInterval(function () {//-------------------------------Consol log test loop that shows past present future logic works.
+        UTCPPF();
+        for (let i = 0; i < WorkHours.length; i++) {
+            console.log(WorkHours[i]);
+            
+        }
+        console.log(CurHMSmSTimeofDay);
+    }, 500);
+
+
+
+
+
 
     // not needed //Find current time - finds the current point in time on spectrum. on transistions or in between
     // 
